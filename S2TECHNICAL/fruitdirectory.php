bd8f1dc8fda300e86afb1122ed66d223996743b3
MathewMacalino-FeuTech <150834159+Zachsxz@users.noreply.github.com>
2026-05-19 11:56:58 +0800

    yup

---
+++ S2TECHNICAL/fruitdirectory.php
@@ -0,0 +1,88 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>My Fruits</title>
+
+    <style>
+
+        body{
+            font-family: Arial;
+        }
+
+        table{
+            width: 95%;
+            margin: auto;
+            border-collapse: collapse;
+            text-align: center;
+        }
+
+        th, td{
+            border: 1px solid black;
+            padding: 10px;
+        }
+
+        img{
+            width: 120px;
+            height: 120px;
+        }
+
+        h2{
+            text-align: center;
+        }
+
+    </style>
+
+</head>
+<body>
+
+<?php
+
+$fruits = array(
+
+array("Apple","Red","Apple is rich in fiber and vitamins.","apple.jpg"),
+array("Banana","Yellow","Banana gives energy and potassium.","banana.jpg"),
+array("Cherry","Red","Cherry contains antioxidants.","cherry.jpg"),
+array("Grapes","Purple","Grapes help heart health.","grapes.jpg"),
+array("Lemon","Yellow","Lemon is rich in Vitamin C.","lemon.jpg"),
+array("Mango","Orange","Mango is the national fruit of the Philippines.","mango.jpg"),
+array("Orange","Orange","Orange improves immunity.","orange.jpg"),
+array("Papaya","Orange","Papaya helps digestion.","papaya.jpg"),
+array("Pineapple","Yellow","Pineapple contains bromelain enzyme.","pineapple.jpg"),
+array("Watermelon","Green","Watermelon contains high water content.","watermelon.jpg")
+
+);
+
+?>
+
+<h2>My Fruits</h2>
+
+<table>
+
+<tr>
+    <th>Image</th>
+    <th>Name</th>
+    <th>Description</th>
+    <th>Facts</th>
+</tr>
+
+<?php
+
+foreach($fruits as $fruit){
+
+echo "<tr>";
+
+echo "<td><img src='".$fruit[3]."'></td>";
+echo "<td>".$fruit[0]."</td>";
+echo "<td>".$fruit[1]."</td>";
+echo "<td>".$fruit[2]."</td>";
+
+echo "</tr>";
+
+}
+
+?>
+
+</table>
+
+</body>
+</html>
