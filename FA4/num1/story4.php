1a84fa249aed9fe24cd996c976b4892be5b9ab7a
MathewMacalino-FeuTech <150834159+Zachsxz@users.noreply.github.com>
2026-05-19 11:43:18 +0800

    DA

--- FA4/num1/story4.php
+++ FA4/num1/story4.php
@@ -1,11 +1,17 @@
 <?php include("header.php"); ?>
+
 <div class="story-box">
-<h1>The Broken Robot</h1>
-<img src="images/story4.jpg">
-<p>
-       A broken robot was left alone in an old junkyard.
-       A young boy repaired the robot and became its best friend.
-       Together, they helped people around their town every day.
-</p>
+
+    <h1>The Broken Robot</h1>
+
+    <img src="images/story4.jpg" alt="The Broken Robot">
+
+    <p>
+        A broken robot was left alone in an old junkyard.
+        A young boy repaired the robot and became its best friend.
+        Together, they helped people around their town every day.
+    </p>
+
 </div>
+
 <?php include("footer.php"); ?>
