b31811a4786bc92a8f254495099b119ce69030c0
MathewMacalino-FeuTech <150834159+Zachsxz@users.noreply.github.com>
2026-05-19 11:58:53 +0800

    da

--- S2TECHNICAL/fruitdirectory.php
+++ S2TECHNICAL/fruitdirectory.php
@@ -39,16 +39,25 @@
 
 $fruits = array(
 
-array("Apple","Red","Apple is rich in fiber and vitamins.","apple.jpg"),
-array("Banana","Yellow","Banana gives energy and potassium.","banana.jpg"),
-array("Cherry","Red","Cherry contains antioxidants.","cherry.jpg"),
-array("Grapes","Purple","Grapes help heart health.","grapes.jpg"),
-array("Lemon","Yellow","Lemon is rich in Vitamin C.","lemon.jpg"),
-array("Mango","Orange","Mango is the national fruit of the Philippines.","mango.jpg"),
-array("Orange","Orange","Orange improves immunity.","orange.jpg"),
-array("Papaya","Orange","Papaya helps digestion.","papaya.jpg"),
-array("Pineapple","Yellow","Pineapple contains bromelain enzyme.","pineapple.jpg"),
-array("Watermelon","Green","Watermelon contains high water content.","watermelon.jpg")
+array("Apple","Red","Apple is rich in fiber and vitamins.","images/apple.jpg"),
+
+array("Banana","Yellow","Banana gives energy and potassium.","images/banana.jpg"),
+
+array("Cherry","Red","Cherry contains antioxidants.","images/cherry.jpg"),
+
+array("Grapes","Purple","Grapes help heart health.","images/grapes.jpg"),
+
+array("Lemon","Yellow","Lemon is rich in Vitamin C.","images/lemon.jpg"),
+
+array("Mango","Orange","Mango is the national fruit of the Philippines.","images/mango.jpg"),
+
+array("Orange","Orange","Orange improves immunity.","images/orange.jpg"),
+
+array("Papaya","Orange","Papaya helps digestion.","images/papaya.jpg"),
+
+array("Pineapple","Yellow","Pineapple contains bromelain enzyme.","images/pineapple.jpg"),
+
+array("Watermelon","Green","Watermelon contains high water content.","images/watermelon.jpg")
 
 );
 
